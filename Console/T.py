841f8bccac3e46fc345adeaabb87166902177086
Kanumuri Sri Ravi Teja <ksriraviteja20@outlook.com>
2023-08-01 08:15:18 +0530

    Update T.py

--- Console/T.py
+++ Console/T.py
@@ -4,7 +4,35 @@ using System.IO;
 using Azure.Storage.Blobs;
 
 public class BlobStorageUploader
+{using Microsoft.CognitiveServices.Speech.Audio;
+
+// ...
+
+// Assuming you already have a regular stream named "inputStream"
+using (var audioInputStream = AudioInputStream.CreatePushStream())
 {
+    // Create an AudioConfig object from the AudioInputStream
+    var audioConfig = AudioConfig.FromStreamInput(audioInputStream);
+
+    // Read data from your original input stream and write it to the AudioInputStream
+    // You can use any buffer size that is appropriate for your application
+    byte[] buffer = new byte[1024];
+    int bytesRead;
+
+    while ((bytesRead = inputStream.Read(buffer, 0, buffer.Length)) > 0)
+    {
+        audioInputStream.Write(buffer, 0, bytesRead);
+    }
+
+    // Optionally, you can close the AudioInputStream when you're done writing data
+    audioInputStream.Close();
+    
+    // Now you can use the audioConfig with your SpeechRecognizer or any other Speech SDK components
+    // For example:
+    // var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+    // var result = await speechRecognizer.RecognizeOnceAsync();
+}
+
     private readonly string connectionString; // Replace with your Azure Storage connection string
     private readonly string containerName; // Replace with your Blob container name
 
